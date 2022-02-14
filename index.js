77300e483601e47c5d2ac3e1fd554c6972b0f322
Jacquan Brooks <jacquanb5@gmail.com>
2022-02-14 17:35:14 -0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+
+let hq = 42;
+
+function distanceFromHqInBlocks(block){
+   if (block > hq){
+  return block - hq; }
+   else {
+    return hq - block;
+  }  
+} 
+
+let feet = 264;
+
+function distanceFromHqInFeet(block) {
+  distanceFromHqInBlocks(block)
+  if (feet > distanceFromHqInBlocks(block)) {
+    return distanceFromHqInBlocks(block) * feet;
+  } else {
+    return feet / distanceFromHqInBlocks(block);
+  }
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  distanceFromHqInFeet(block);
+  if (destination > start) {
+    return destination - start * distanceFromHqInFeet
+  }
+    else  {
+    return start - destination * distanceFromHqInFeet 
+  }
+}
+
+
+
+
